1685f21b7c0a99662edba4c999f7e9a8b903275c
prinsi-t <tprincy56@gmail.com>
2026-09-20 00:42:31 +0530

    new challenge

--- addBinary.js
+++ addBinary.js
@@ -4,7 +4,13 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    
-    return (parseInt(a, 2) + parseInt(b, 2)).toString(2);
+    let i = a.length - 1, j = b.length - 1, carry = 0, result = '';
 
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = Number(a[i--] || 0) + Number(b[j--] || 0) + carry;
+        result = sum % 2 + result;
+        carry = Math.floor(sum / 2);
+    }
+
+    return result;
 };
